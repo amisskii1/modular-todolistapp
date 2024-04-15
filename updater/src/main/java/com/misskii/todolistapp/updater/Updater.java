2f63985b9ebb1e39d1fa621c30f96761219483a3
Anton Misskii <anton.misskii@gmail.com>
2024-04-15 22:40:17 +0200

    refactor updater module

--- updater/src/main/java/com/misskii/todolistapp/updater/Updater.java
+++ updater/src/main/java/com/misskii/todolistapp/updater/Updater.java
@@ -1,6 +1,7 @@
 package com.misskii.todolistapp.updater;
 
 import java.io.*;
+
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.misskii.todolistapp.updater.api.UpdaterApi;
@@ -11,34 +12,34 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 public class Updater implements UpdaterApi {
-    private static final String ACTUAL_VERSION = "1.0.4";
+    private static final String ACTUAL_VERSION = "1.1.2";
     private final String gitToken = System.getenv("packages_token");
     private String latestVersion;
-   public boolean compareVersions(){
-       try {
-           String apiUrl = "https://api.github.com/users/AntonMisskii/packages/maven/com.misskii.javatodolistapp/versions";
-           HttpClient httpClient = HttpClients.createDefault();
-           HttpGet httpGet = new HttpGet(apiUrl);
-           httpGet.addHeader("Accept", "application/vnd.github+json");
-           httpGet.addHeader("Authorization", "Bearer "+ gitToken);
-           httpGet.addHeader("X-GitHub-Api-Version", "2022-11-28");
-           HttpResponse response = httpClient.execute(httpGet);
+    public boolean compareVersions(){
+        try {
+            String apiUrl = "https://api.github.com/users/amisskii1/packages/maven/com.misskii.javatodolistapp/versions";
+            HttpClient httpClient = HttpClients.createDefault();
+            HttpGet httpGet = new HttpGet(apiUrl);
+            httpGet.addHeader("Accept", "application/vnd.github+json");
+            httpGet.addHeader("Authorization", "Bearer "+ gitToken);
+            httpGet.addHeader("X-GitHub-Api-Version", "2022-11-28");
+            HttpResponse response = httpClient.execute(httpGet);
 
-           String responseBody = EntityUtils.toString(response.getEntity());
-           ObjectMapper objectMapper = new ObjectMapper();
-           JsonNode jsonNode = objectMapper.readTree(responseBody);
-           String latestVersion = jsonNode.get(0).get("name").asText();
+            String responseBody = EntityUtils.toString(response.getEntity());
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(responseBody);
+            String latestVersion = jsonNode.get(0).get("name").asText();
 
-           setLatestVersion(latestVersion);
+            setLatestVersion(latestVersion);
 
-           if (ACTUAL_VERSION.equals(latestVersion)) {
-               return true;
-           }
-       } catch (IOException e) {
-           e.printStackTrace();
-       }
-       return false;
-   }
+            if (ACTUAL_VERSION.equals(latestVersion)) {
+                return true;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
     public String getActualVersion() {
         return ACTUAL_VERSION;
